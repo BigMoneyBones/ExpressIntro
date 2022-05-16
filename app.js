1a7b0a12b2006e0579647106cb6e92191d3c70cd
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-05-16 14:52:09 -0500

    fix search

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const app = express();
+const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Jesse Raines')
-})
+// Always keep this one at the top
+app.get("/", (req, res) => {
+  res.send("Jesse Raines");
+});
 
-app.get('search', (req, res) => {
-    res.statusMessage(201).send('yes this is a page!')
-})
+app.get("/search", (req, res) => {
+  res.statusMessage(201).send("yes this is a page!");
+});
 
-app.get('/about', (req, res) => {
-    res.send('This is where you learn about me!')
-})
+app.get("/about", (req, res) => {
+  res.send("This is where you learn about me!");
+});
 
-app.get('/contact', (req, res) => {
-    res.send('Here you will get my contact info!')
-})
+app.get("/contact", (req, res) => {
+  res.send("Here you will get my contact info!");
+});
 
-app.get('/hobbies', (req, res) => {
-    res.send('now you finna see my hobbies/interests')
-})
+app.get("/hobbies", (req, res) => {
+  res.send("now you finna see my hobbies/interests");
+});
 
-app.get('*', (req, res) => {
-    res.status(404).send()
-})
+// Wildcard catchall -  always keep this at the bottom. '*' will be anything not specifically listed.
+app.get("*", (req, res) => {
+  res.status(404).send();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
